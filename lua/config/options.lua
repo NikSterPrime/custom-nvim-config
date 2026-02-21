786ac60d40db8b8dc4389d5e05d9f6663a09423c
Adityas-Macbook-Air <120474074+NikSterPrime@users.noreply.github.com>
2026-02-21 10:14:23 +0530

    Added lsp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,12 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- Non-current windows
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" }) -- Sign column (left)
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" }) -- Line numbers
+
 
 
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,5 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.config",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "diff",
-        "html",
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "vim",
-        "vimdoc",
-	"javascript",
-	"python",
-	"java",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = {
-        enable = true,
-        disable = { "ruby" },
-      },
-    },
-  },
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate'
 }
-
